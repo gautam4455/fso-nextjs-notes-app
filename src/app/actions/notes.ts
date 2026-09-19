2590d4d2b20f20f9ca6622880c4a051cbc53fd24
gautam <gautam.kamble4455@gmail.com>
2026-09-19 13:41:05 +0530

    Added get notes, add note using server components and server actions

---
+++ src/app/actions/notes.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+import { addNote } from "../services/notes";
+
+export const createNote = async (formData: FormData) => {
+  const content = formData.get("content") as string;
+  const important = formData.get("important") === "on";
+
+  addNote(content, important);
+  redirect("/notes");
+};
