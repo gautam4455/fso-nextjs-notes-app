2394b28f2ee9ac353aefda2a6bc755363ffdd744
gautam <gautam.kamble4455@gmail.com>
2026-09-19 16:56:59 +0530

    Switched to server component for showing important notes

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,14 +1,44 @@
-import { NoteList } from "@/app/notes/NoteList";
+import Link from "next/link";
+
+// import { NoteList } from "@/app/notes/NoteList";
 import { getNotes } from "../services/notes";
 
-const Notes = () => {
-  const notes = getNotes();
+const Notes = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ important?: string }>;
+}) => {
+  const { important } = await searchParams;
+  const showImportant = important === "true";
+
+  const allNotes = getNotes();
+  const notes = showImportant
+    ? allNotes.filter((note) => note.important)
+    : allNotes;
 
   return (
     <div>
       <h2>Notes</h2>
 
-      <NoteList notes={notes} />
+      {/* Client Component Solution */}
+      {/* <NoteList notes={allNotes} /> */}
+
+      {/* Server Somponent Solution */}
+      <div>
+        <Link href={showImportant ? "/notes" : "/notes?important=true"}>
+          {showImportant ? "show all" : "show important only"}
+        </Link>
+      </div>
+
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <Link href={`/notes/${note.id}`}>{note.content}</Link>
+
+            {note.important && <strong> (important)</strong>}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
